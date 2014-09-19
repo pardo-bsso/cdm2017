d7e14dcb76040303c3f6ed77d38922e8d7141a6a
Adrian Pardini <publico@tangopardo.com.ar>
2014-09-18 22:49:49 -0300

    tp2e7: agregado comentarios

--- tp2/tp2e7.asm
+++ tp2/tp2e7.asm
@@ -1,3 +1,9 @@
+; Apunto X al final de la cadena
+; X = 0x0081 + len - 1
+; Recorro para atras, me aprovecho que al cargar A con 0
+; beq branchea.
+; Cuando X apunta a 0x0080 ya termine.
+
         org $80
 
 len     db !10
@@ -11,12 +17,14 @@ str     db 'abc'
         org $EE00
 start
 
+; X apunta al final de la cadena. Ver arriba.
         lda $80
         add #$81
         deca
         tax
 
 loop
+; Cargo en A elemento actual, si es 0 lo reemplazo por 0x20.
         lda ,X
         beq repl
         jmp loop_tail
@@ -26,6 +34,7 @@ repl
         sta ,X
 
 loop_tail
+; Apunto X al elemento anterior. Si es 0x0080 ya termine.
         decx
         cpx #$80
         bgt loop
