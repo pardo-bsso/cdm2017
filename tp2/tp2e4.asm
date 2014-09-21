c7c6f42d8ffe0f3b448c330660d53a80698b58c9
Adrian Pardini <publico@tangopardo.com.ar>
2014-09-21 08:14:26 -0300

    tp2e4: agregado comentarios

--- tp2/tp2e4.asm
+++ tp2/tp2e4.asm
@@ -1,9 +1,20 @@
+; Sumar tres numeros de 16 bits (na, nb, nc)
+; En este caso el acarreo en byte alto es ignorado, dando resultados erroneos
+; si en algun paso la suma excede 0xFFFF.
+;
+; El proceso se hace por partes, sumando primero dos bytes bajos y luego dos
+; altos mas el carry de la primer operacion, quedando el resultado en la
+; variable de salida.
+; Esto se repite entre el resultado intermedio y el numero de entrada restante.
+
         org $80
 na      dw $DEAD
 nb      dw $BEEF
 nc      dw $BADC
 res     rmb 2
 
+; Atajos para acceder al byte alto/bajo de cada parametro y hacer, un poco,
+; mas legible el resto.
 naH     equ na
 naL     equ na+1
 nbH     equ nb
@@ -15,15 +26,20 @@ resL    equ res+1
 
         org $EE00
 start
+; Borro carry, sumo byte bajo de na y nb.
+; Resultado en variable de salida.
         clc
         lda naL
         adc nbL
         sta resL
 
+; Sumo bytes altos de na y nb mas el carry de la operacion anterior.
+; Resultado en variable de salida.
         lda naH
         adc nbH
         sta resH
 
+; Igual que antes pero entre el resultado y nc.
         clc
         lda ncL
         adc resL
