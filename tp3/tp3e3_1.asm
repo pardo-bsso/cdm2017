d9cfda63ac7c5e324e88ad841b4c7fd141684f7a
Adrian Pardini <publico@tangopardo.com.ar>
2014-10-02 08:50:09 -0300

    tp3e3_1: cycle count.

--- tp3/tp3e3_1.asm
+++ tp3/tp3e3_1.asm
@@ -17,41 +17,47 @@ Rh      db $00
 start
 ;                            cycles.
 
-        ldx #$10
-        clr R
-        clr Rh
-        clr C
-        clra
+        ldx #$10            ; 2
+        clr R               ; 3
+        clr Rh              ; 3
+        clr C               ; 3
+        clra                ; 1
+                            ; 12 total.
 
 loop_bits
-        clc
-        rol A+1
-        rol A
-        rol R
-        rol Rh
-        asl C
+        clc                 ; 1
+        rol A+1             ; 4
+        rol A               ; 4
+        rol R               ; 4
+        rol Rh              ; 4
+        asl C               ; 4
+                            ; 21 total.
 
-        lda Rh
-        bne resta
-        lda R
-        cmp B
-        blo loop_bits_fin
+        lda Rh              ; 3
+        bne resta           ; 3
+        lda R               ; 3
+        cmp B               ; 3
+        blo loop_bits_fin   ; 3
+                            ; 15 total.
 
 resta
-        lda R
-        sub B
-        sta R
-        lda Rh
-        sbc #$00
-        sta Rh
-        bset 0,C
+        lda R               ; 3
+        sub B               ; 3
+        sta R               ; 3
+        lda Rh              ; 3
+        sbc #$00            ; 2
+        sta Rh              ; 3
+        bset 0,C            ; 4
+                            ; 21 total.
 
 
 loop_bits_fin
 
+        decx                ; 1
+        bne loop_bits       ; 3
 
-        decx
-        bne loop_bits
+                            ; 73 total peor caso por iteracion.
+                            ; 1168 (16 iteraciones, una por bit) peor caso.
 
 
 fin
