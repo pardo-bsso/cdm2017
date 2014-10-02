60cdd1c28b9f697fa07d23a4c4ccbdf1860d9b48
Adrian Pardini <publico@tangopardo.com.ar>
2014-10-02 09:47:12 -0300

    tp1e3: corregido valores de ccr

--- tp1/tp1e3.asm
+++ tp1/tp1e3.asm
@@ -2,22 +2,22 @@
 
         org $EE00
 start
-;       nem                  addr   opc(hex)    dir         ccr
-        ldhx #$0080         ;EE00   450080      inmediato   -11-I---
-        lda #$90            ;EE03   A690        inmediato   -11-IN--
-        psha                ;EE05   87          inherente   =
-        lda ,X              ;EE06   F6          indexado    -11-I---
-        bit #$01            ;EE07   A501        inmediato   011--??-
-        beq *+0C            ;EE09   270A        relativo    -11-----
-        pshx                ;EE0B   89          inherente   -11-----
-        ldx $2,sp           ;EE0C   9EEE02      indexado SP 011--??-
-        sta ,X              ;EE0F   F7          inherente   011--??-
-        incx                ;EE10   5C          inherente   011--?--
-        stx $2,sp           ;EE11   9EEF02      indexado SP 011--??-
-        pulx                ;EE14   88          inherente   011-----
-        incx                ;EE15   5C          inherente   011--?--
-        cphx #$0090         ;EE16   650090      inmediato   011--???
-        bne *-13            ;EE19   26EB        relativo    -11-----
+;       nem                  addr   opc(hex)    dir         ccr (solo primer iteracion)
+        ldhx #$0080         ;EE00   450080      inmediato   01101000
+        lda #$90            ;EE03   A690        inmediato   01101100
+        psha                ;EE05   87          inherente   01101100
+        lda ,X              ;EE06   F6          indexado    01101100
+        bit #$01            ;EE07   A501        inmediato   01101000
+        beq *+0C            ;EE09   270A        relativo
+        pshx                ;EE0B   89          inherente   01101000
+        ldx $2,sp           ;EE0C   9EEE02      indexado SP 01101100
+        sta ,X              ;EE0F   F7          inherente   01101100
+        incx                ;EE10   5C          inherente   01101100
+        stx $2,sp           ;EE11   9EEF02      indexado SP 01101100
+        pulx                ;EE14   88          inherente   01101100
+        incx                ;EE15   5C          inherente   01101100
+        cphx #$0090         ;EE16   650090      inmediato   01101101
+        bne *-13            ;EE19   26EB        relativo
 
 end
         jmp end             ;
