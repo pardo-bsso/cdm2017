5a3cc0f8f3205501516e3d04ee9fba7db1a3d6d0
Adrian Pardini <publico@tangopardo.com.ar>
2014-10-02 09:42:36 -0300

    tp1e1: corregido valores de ccr

--- tp1/tp1e1.asm
+++ tp1/tp1e1.asm
@@ -2,14 +2,14 @@
 
         org $EE00
 start
-;       nem                  addr   opc(hex)    dir         ccr
-        clr $80             ;EE00   3F80        directo     011--01-
-        lda #$08            ;EE02   A608        inmediato   011--  -
-        sta $81             ;EE04   B781        directo     011--  -
-        lda #%10010110      ;EE06   A696        inmediato   011--10-
-        rola                ;EE08   49          inherente   111----1
-        ror $80             ;EE09   3680        directo     111--1--
-        dec $81             ;EE0B   3A81        directo     
+;       nem                  addr   opc(hex)    dir         ccr (solo primer iteracion)
+        clr $80             ;EE00   3F80        directo     01100010
+        lda #$08            ;EE02   A608        inmediato   01100000
+        sta $81             ;EE04   B781        directo     01100000
+        lda #%10010110      ;EE06   A696        inmediato   01100100
+        rola                ;EE08   49          inherente   11100001
+        ror $80             ;EE09   3680        directo     11101100
+        dec $81             ;EE0B   3A81        directo     01101000
 ; PC = PC + 0x0002 + rel
 ; rel = EE08 - (EE0D + 0x0002) = -7
 ; rel = F9
